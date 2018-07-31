c66216a5eded055a562a67d2ee6457cae4f26fc7
PhuongTntechs <kid.tn01@gmail.com>
2018-07-31 14:10:12 +0700

    first push

---
+++ PHParalax/PHParalax/PHParalax.h
@@ -0,0 +1,19 @@
+//
+//  PHParalax.h
+//  PHParalax
+//
+//  Created by phuongpro Imac on 7/31/18.
+//  Copyright © 2018 phuongpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PHParalax.
+FOUNDATION_EXPORT double PHParalaxVersionNumber;
+
+//! Project version string for PHParalax.
+FOUNDATION_EXPORT const unsigned char PHParalaxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PHParalax/PublicHeader.h>
+
+
